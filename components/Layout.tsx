51f09f8498853963cadbe5b0180bc23e3c78055e
saphir746 <ds711@ic.ac.uk>
2025-07-21 11:14:47 +0100

    Name change

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({ children }: LayoutProps) {
           <div className="flex justify-between h-16">
             <div className="flex items-center">
               <Link href="/" className="text-xl font-bold">
-                TrekCo
+                HikeinHimalaya
               </Link>
             </div>
             <div className="flex items-center space-x-4">
